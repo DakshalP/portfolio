645a662f8d156d5550680d005d931c2d87344e2e
DakshalP <dakshal29@gmail.com>
2021-05-23 17:29:36 -0400

    add project sorting functionality using tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,119 @@
-import React from "react"
+import React, { useState } from 'react';
 import Helmet from 'react-helmet';
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
-import PostLink from "../components/post-link"
-import HeroHeader from "../components/heroHeader"
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+import PostLink from '../components/post-link';
+import HeroHeader from '../components/heroHeader';
+import MultiSelect from 'react-multi-select-component';
 
 const IndexPage = ({
-  data: {
-    site,
-    allMarkdownRemark: { edges },
-  },
+    data: {
+        site,
+        allMarkdownRemark: { edges },
+    },
 }) => {
+    const [selectedTags, setSelectedTags] = useState([]);
 
-  const Posts = edges
-    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+    const Posts = edges
+        .filter((edge) => !!edge.node.frontmatter.tags) // You can filter your posts based on some criteri
+        .filter((edge) => {
+            if (selectedTags.length > 0)
+                for (let obj of selectedTags) {
+                    if (edge.node.frontmatter.tags.includes(obj.value))
+                        return true;
+                }
+            else return true;
+        })
+        .map((edge) => <PostLink key={edge.node.id} post={edge.node} />);
 
-  return (
-    <Layout>
-      <Helmet>
-        <title>{site.siteMetadata.title}</title>
-        <meta name="description" content={site.siteMetadata.description} />
-      </Helmet>
-      <HeroHeader/>
-      <h2 id="projects-header">Projects</h2>
-      <div className="grids">
-        {Posts}
-      </div>
-    </Layout>
-  )
-}
+    const tagOptions = [
+        { label: 'Arduino', value: 'arduino' },
+        { label: 'React', value: 'react' },
+        { label: 'Web', value: 'web' },
+        { label: 'C#', value: 'C#' },
+    ];
 
-export default IndexPage
+    const customValueRenderer = (selected, _options) => {
+        return selected.length
+            ? selected.map(({ label }) => (
+                  <span className="tag-label">{label}</span>
+              ))
+            : 'Sort by tags...';
+    };
+    const CustomItemRenderer = ({ checked, option, onClick, disabled }) => (
+        <div className={`item-renderer ${disabled && 'disabled'}`}>
+            <input
+                type="checkbox"
+                onChange={onClick}
+                checked={checked}
+                tabIndex={-1}
+                disabled={disabled}
+                style={{ display: 'none' }}
+            />
+            <span>{option.label}</span>
+        </div>
+    );
+    const Arrow = () => <></>;
+
+    return (
+        <Layout>
+            <Helmet>
+                <title>{site.siteMetadata.title}</title>
+                <meta
+                    name="description"
+                    content={site.siteMetadata.description}
+                />
+            </Helmet>
+            <HeroHeader />
+            <h2 id="projects-header">Projects</h2>
+            <div className="tag-picker">
+                <MultiSelect
+                    options={tagOptions}
+                    value={selectedTags}
+                    onChange={setSelectedTags}
+                    labelledBy="Select tags"
+                    disableSearch
+                    overrideStrings={{
+                        allItemsAreSelected: 'All projects.',
+                        selectAll: 'All',
+                        selectSomeItems: 'Sort by tag...',
+                    }}
+                    ClearSelectedIcon={<i id="clear">✖</i>}
+                    ArrowRenderer={Arrow}
+                    ItemRenderer={CustomItemRenderer}
+                    valueRenderer={customValueRenderer}
+                    hasSelectAll={false}
+                />
+            </div>
+
+            <div className="grids">{Posts}</div>
+        </Layout>
+    );
+};
+
+export default IndexPage;
 export const pageQuery = graphql`
-  query indexPageQuery {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-            thumbnail
-            tags
-          }
+    query indexPageQuery {
+        site {
+            siteMetadata {
+                title
+                description
+            }
         }
-      }
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+            edges {
+                node {
+                    id
+                    excerpt(pruneLength: 250)
+                    frontmatter {
+                        date(formatString: "MMMM DD, YYYY")
+                        path
+                        title
+                        thumbnail
+                        tags
+                    }
+                }
+            }
+        }
     }
-  }
-`
+`;
