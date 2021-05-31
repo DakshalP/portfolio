6f81e5834ba27b4e123626147376c6e1f64eddad
DakshalP <dakshal29@gmail.com>
2021-05-30 22:05:17 -0400

    fix cards uneven issue

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -2,33 +2,33 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const PostLink = ({ post }) => (
-    <Link to={post.frontmatter.path}>
-        <article className="card ">
+    <article className="card ">
+        <Link to={post.frontmatter.path}>
             {!!post.frontmatter.thumbnail && (
                 <img
                     src={post.frontmatter.thumbnail}
                     alt={post.frontmatter.title + '- Featured Shot'}
                 />
             )}
+        </Link>
 
-            <header>
-                <h2 className="post-title">
-                    <Link to={post.frontmatter.path} className="post-link">
-                        {post.frontmatter.title}
-                    </Link>
-                </h2>
-                <div className="post-meta">{post.frontmatter.tagline}</div>
-                <div className="post-tags">
-                    {post.frontmatter.tags
-                        ? post.frontmatter.tags.split(',').map((tag) => (
-                              <span className="post-tag" key={tag}>
-                                  {tag}
-                              </span>
-                          ))
-                        : null}
-                </div>
-            </header>
-        </article>
-    </Link>
+        <header>
+            <h2 className="post-title">
+                <Link to={post.frontmatter.path} className="post-link">
+                    {post.frontmatter.title}
+                </Link>
+            </h2>
+            <div className="post-meta">{post.frontmatter.tagline}</div>
+            <div className="post-tags">
+                {post.frontmatter.tags
+                    ? post.frontmatter.tags.split(',').map((tag) => (
+                          <span className="post-tag" key={tag}>
+                              {tag}
+                          </span>
+                      ))
+                    : null}
+            </div>
+        </header>
+    </article>
 );
 export default PostLink;
