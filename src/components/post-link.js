d879da835bfe3630c1226efac8b430ffb017c79f
DakshalP <dakshal29@gmail.com>
2021-05-30 22:00:51 -0400

    make posts cards easier to click

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -2,32 +2,33 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const PostLink = ({ post }) => (
-    <article className="card ">
-        <Link to={post.frontmatter.path}>
+    <Link to={post.frontmatter.path}>
+        <article className="card ">
             {!!post.frontmatter.thumbnail && (
                 <img
                     src={post.frontmatter.thumbnail}
                     alt={post.frontmatter.title + '- Featured Shot'}
                 />
             )}
-        </Link>
-        <header>
-            <h2 className="post-title">
-                <Link to={post.frontmatter.path} className="post-link">
-                    {post.frontmatter.title}
-                </Link>
-            </h2>
-            <div className="post-meta">{post.frontmatter.tagline}</div>
-            <div className="post-tags">
-                {post.frontmatter.tags
-                    ? post.frontmatter.tags.split(',').map((tag) => (
-                          <span className="post-tag" key={tag}>
-                              {tag}
-                          </span>
-                      ))
-                    : null}
-            </div>
-        </header>
-    </article>
+
+            <header>
+                <h2 className="post-title">
+                    <Link to={post.frontmatter.path} className="post-link">
+                        {post.frontmatter.title}
+                    </Link>
+                </h2>
+                <div className="post-meta">{post.frontmatter.tagline}</div>
+                <div className="post-tags">
+                    {post.frontmatter.tags
+                        ? post.frontmatter.tags.split(',').map((tag) => (
+                              <span className="post-tag" key={tag}>
+                                  {tag}
+                              </span>
+                          ))
+                        : null}
+                </div>
+            </header>
+        </article>
+    </Link>
 );
 export default PostLink;
