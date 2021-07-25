9c86f47a03e3a2e9f60870a7ae574cb3050f1c77
DakshalP <dakshal29@gmail.com>
2021-07-25 14:33:15 -0400

    fix image scaling

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -5,22 +5,26 @@ const PostLink = ({ post }) => (
     <article className="card ">
         <Link to={post.frontmatter.path}>
             {!!post.frontmatter.thumbnail && (
-                <img
-                    src={post.frontmatter.thumbnail}
+                <div
+                    id="featuredImage"
+                    style={{
+                        backgroundImage: `url(${post.frontmatter.thumbnail})`,
+                    }}
+                    // src={post.frontmatter.thumbnail}
                     alt={post.frontmatter.title + '- Featured Shot'}
                 />
             )}
         </Link>
 
         <header>
-            <h2 className="post-title">
+            <h3 className="post-title">
                 <Link
                     to={post.frontmatter.path}
                     className="post-link hover underline"
                 >
                     {post.frontmatter.title}
                 </Link>
-            </h2>
+            </h3>
             <div className="post-tagline">{post.frontmatter.tagline}</div>
             <div className="post-tags">
                 {post.frontmatter.tags
