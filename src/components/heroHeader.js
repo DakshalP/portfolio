d58c43b89f3197d8d34acfe97b0c18d51ad1f2ea
DakshalP <dakshal29@gmail.com>
2021-07-24 22:20:21 -0400

    add commit button and style

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -17,10 +17,19 @@ export default () => (
         render={(data) => (
             <div className="hero-header">
                 <div className="headline">
-                Hello! I'm <a href="https://www.linkedin.com/in/dakshalp/" id="name">Dakshal Panicker</a>. Here
-                    you can browse some of my{' '}
-                    <Link id="projects" to="/#projects-header">projects</Link> or write a {' '}
-                    <Link to="/contact" id="contact">message</Link> to me. 
+                    Hello! I'm{' '}
+                    <a href="https://www.linkedin.com/in/dakshalp/" id="name">
+                        Dakshal Panicker
+                    </a>
+                    . Here you can browse some of my{' '}
+                    <Link id="projects" to="/#projects-header">
+                        projects
+                    </Link>{' '}
+                    or write a{' '}
+                    <Link to="/contact" id="contact">
+                        message
+                    </Link>{' '}
+                    to me.
                 </div>
                 <div
                     className="primary-content"
@@ -28,9 +37,23 @@ export default () => (
                     //     __html: data.site.siteMetadata.home.description,
                     // }}
                 />
-                <a href="https://www.linkedin.com/in/dakshalp/" className="button -primary"> LinkedIn </a>
-                <a href="https://github.com/DakshalP" className="button -primary"> Github </a>
-
+                <Link to="/contact" className="button -secondary">
+                    Contact
+                </Link>
+                <a
+                    href="https://www.linkedin.com/in/dakshalp/"
+                    className="button -primary"
+                >
+                    {' '}
+                    LinkedIn{' '}
+                </a>
+                <a
+                    href="https://github.com/DakshalP"
+                    className="button -primary"
+                >
+                    {' '}
+                    Github{' '}
+                </a>
             </div>
         )}
     />
