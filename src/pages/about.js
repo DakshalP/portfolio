296c84b7cdce639d42367a4d3e624c0d3c570e01
Dakshal P <dakshal29@gmail.com>
2023-05-14 11:11:08 -0400

    add some of my photos

--- src/pages/about.js
+++ src/pages/about.js
@@ -88,39 +88,31 @@ const AboutPage = ({ data: { site } }) => (
                     <SiNumpy />
                     <SiR />
                 </div> */}
-
-                {/* image gallery */}
             </div>
         </div>
         <div className="image-container">
             <h2>Work in progress gallery</h2>
             <ul className="gallery">
                 <li>
-                    <img src="https://source.unsplash.com/VWcPlbHglYc" alt="" />
+                    <img src="/assets/nature_photos/1.jpg" alt="" />
                     <div class="overlay">
                         <span>Image title</span>
                     </div>
                 </li>
                 <li>
-                    <img src="https://source.unsplash.com/e6FMMambeO4" alt="" />
+                    <img src="/assets/nature_photos/2.jpg" alt="" />
                     <div class="overlay">
                         <span>Image title</span>
                     </div>
                 </li>
                 <li>
-                    <img src="https://source.unsplash.com/klCiPmzUw0Y" alt="" />
+                    <img src="/assets/nature_photos/3.jpg" alt="" />
                     <div class="overlay">
                         <span>Image title</span>
                     </div>
                 </li>
                 <li>
-                    <img src="https://source.unsplash.com/IdNOTjPeHrE" alt="" />
-                    <div class="overlay">
-                        <span>Image title</span>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://source.unsplash.com/O0N9MF--hK4" alt="" />
+                    <img src="/assets/nature_photos/4.jpeg" alt="" />
                     <div class="overlay">
                         <span>Image title</span>
                     </div>
